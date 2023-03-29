bb90f72b8d5fb91c9e9c612aa3da02aeb6d2f8c9
xeroxis-xs <txsheng0609@hotmail.com>
2023-03-29 18:36:30 +0800

    updated some driver for pyodbc

---
+++ README.md
@@ -0,0 +1,3 @@
+need to install the correct pyodbc driver
+MacOS homebrew
+https://learn.microsoft.com/th-th/sql/connect/odbc/linux-mac/install-microsoft-odbc-driver-sql-server-macos?view=sql-server-2017
--- sqlconn.py
+++ sqlconn.py
@@ -1,6 +1,17 @@
 import pyodbc as odbc
 
-DRIVER_NAME = 'SQL SERVER'
+if odbc.drivers():
+    print("--------------------------------------")
+    print("List of Pyodbc driver in your machine:")
+    for i in range(len(odbc.drivers())):
+        print(i+1, odbc.drivers()[i])
+    sql_driver = odbc.drivers()[0]
+    print(f"Using {sql_driver}")
+    print("--------------------------------------")
+else:
+    print("No Pyodbc driver in your machine, please install one before continuing.")
+
+DRIVER_NAME = f'{sql_driver}'
 SERVER_NAME = 'database-1.ctfix6axgfer.ap-southeast-2.rds.amazonaws.com'
 DATABASE_NAME = 'Bookstore'
 UID = 'txsheng0609'
@@ -18,6 +29,7 @@ class SQL:
             DATABASE={DATABASE_NAME};
             UID={UID};
             PWD={PWD};
+            TrustServerCertificate=yes;
         """
         self.conn = odbc.connect(connection_string)
         self.cursor = self.conn.cursor()
